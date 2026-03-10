89b76623c4974f3a695d4e116513ee12f44c4466
Sara Davies <smhumphries@hotmail.co.uk>
2026-03-10 13:09:01 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,12 +3,12 @@ provider "aws" {
 }
 terraform {
   backend "s3" {
-    bucket         = "{your state bucket}"   
+    bucket         = "jenkins-state-sara-davies-10-03"   
     key            = "terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
   }
 }
 resource "aws_s3_bucket" "example" {
-bucket = "jenkins-test-bucket-{yournamehere}"
+bucket = "jenkins-test-bucket-sara-davies-10-03
 }
